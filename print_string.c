812c0f75d07e2f7a89e1a33947b1a39963f6f3e0
Zainab Hossien <140790812+ZainabFadil@users.noreply.github.com>
2023-09-15 17:02:47 +0300

    printf-archeticture-file-part2 (#3)

---
+++ print_string.c
@@ -0,0 +1,16 @@
+#include <stdarg.h>
+#include "main.h"
+/**
+* print_string - print string
+*@string_arg: pointer to the corresponding argument
+*Return: return length of the printed string
+*
+*/
+int print_string(va_list  string_arg)
+{
+	/**to do va_arg and print it*/
+	if (string_arg)
+		return (1);
+	else
+		return (0);
+}
