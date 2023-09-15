af5cfe5d573595992a70e112cc4bfc22a9bb8998
ZainabFadil <ugs.19687@ci.suez.edu.eg>
2023-09-15 16:36:54 +0300

    seven architecture files added

---
+++ print_character.c
@@ -0,0 +1,16 @@
+#include <stdarg.h>
+#include "main.h"
+/**
+* print_character - print character
+*@char_arg: pointer to the corresponding argument
+*Return: return length of the printed char
+*
+*/
+int print_character(va_list  char_arg)
+{
+    /**to do va_arg and print it*/
+	if (char_arg)
+		return (1);
+	else
+		return (0);
+}
---
+++ print_integer.c
@@ -0,0 +1,16 @@
+#include <stdarg.h>
+#include "main.h"
+/**
+* print_integer - print integer
+*@integer_arg: pointer to the corresponding argument
+*Return: return length of the printed integer
+*
+*/
+int print_integer(va_list  integer_arg)
+{
+   /**to do va_arg and print it*/
+	if (integer_arg)
+		return (1);
+	else
+		return (0);
+}
