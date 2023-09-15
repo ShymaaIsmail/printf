a45a3c8096bdf0e5537923a0ad09c3dd7f4c397f
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-09-15 20:03:24 +0300

    printf-archeticture-file-part2 (#3) (#4)
    Co-authored-by: Zainab Hossien <140790812+ZainabFadil@users.noreply.github.com>

---
+++ print_digit.c
@@ -0,0 +1,16 @@
+#include <stdarg.h>
+#include "main.h"
+/**
+* print_digit - print digit
+*@digit_arg: pointer to the corresponding argument
+*Return: return length of the printed digit
+*
+*/
+int print_digit(va_list  digit_arg)
+{
+    /**to do va_arg and print it*/
+	if (digit_arg)
+		return (1);
+	else
+		return (0);
+}
---
+++ print_percent.c
@@ -0,0 +1,12 @@
+#include <stdarg.h>
+#include "main.h"
+/**
+* print_percent - print percent sign
+* Return: alway return 1
+*
+*/
+int print_percent(void)
+{
+	_putchar('%');
+	return (1);
+}
