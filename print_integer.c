3c0a148a78ae6b114335a2811674848a10dfbc58
ZainabFadil <ugs.19687@ci.suez.edu.eg>
2023-09-16 21:52:54 +0300

    first brain storming

--- print_integer.c
+++ print_integer.c
@@ -1,16 +1,20 @@
 #include <stdarg.h>
 #include "main.h"
 /**
-* print_integer - print integer
-*@integer_arg: pointer to the corresponding argument
-*Return: return length of the printed integer
-*
-*/
+ * print_integer - print the given integer, handles the %d and %i
+ * specifiers in the printf ffunction
+ * @integer_arg: the given argument that I want to print
+ * Return: (int) the length of the given decimal
+ */
 int print_integer(va_list  integer_arg)
 {
-   /**to do va_arg and print it*/
-	if (integer_arg)
-		return (1);
-	else
-		return (0);
+	int num = va_arg(integer_arg);
+	int cnt = 0;
+
+	while (num)
+	{
+		count += print_digit(integer_arg);
+		num /= 10;
+	}
+	return (cnt);
 }
