fa4296b4ed8e1e34073a61a6384fa24e49780494
shymaaismail <shymaa.m.ismail@gmail.com>
2023-09-16 15:20:47 +0300

    handle all % cases

--- print_and_count.c
+++ print_and_count.c
@@ -7,13 +7,13 @@
  * @arg_list: arg_list
  * @format_current_index: format_current_index
  * @count_chars: count_chars
+ * @arr_result: arr_result
  * Return: array of int for (count char and format cuurent index)
  *
 */
-int *handle_indicator(char *next_char, va_list arg_list,
+void handle_indicator(int *arr_result, char next_char, va_list arg_list,
 						int format_current_index, int count_chars)
 {
-	int *arr_result[2];
 	int indicator_index = 0, is_handeled_token = 0;
 	arg_indicator_type indicators[] = { {"%", print_percent},
 	{"c", print_character}, {"s", print_string}, {"d", print_digit},
@@ -23,9 +23,9 @@ int *handle_indicator(char *next_char, va_list arg_list,
 	{
 		if (next_char == *indicators[indicator_index].token)
 		{
-				count_chars += indicators[indicator_index].print(arg_list);
-				is_handeled_token = 1;
-				break;
+			count_chars += indicators[indicator_index].print(arg_list);
+			is_handeled_token = 1;
+			break;
 		}
 			indicator_index++;
 		}
@@ -33,20 +33,22 @@ int *handle_indicator(char *next_char, va_list arg_list,
 				format_current_index += 2;
 			else
 				{
-					if (next_char == "%")
+					if (next_char == '%')
 					{
 						count_chars += print_percent();
 						format_current_index += 2;
 					}
-					else
+					else if (next_char != '\0')
 					{
 						count_chars += print_percent();
 						format_current_index += 1;
 					}
-			}
+					else
+						format_current_index += 1;
+
+				}
 		arr_result[0] = count_chars;
 		arr_result[1] = format_current_index;
-		return (arr_result);
 }
 /**
  * print_and_count - print formatted text and count its chars
@@ -57,21 +59,20 @@ int *handle_indicator(char *next_char, va_list arg_list,
 */
 int print_and_count(va_list arg_list, const char *format)
 {
-	int count_chars = 0, indicator_index = 0, is_handeled_token = 0;
+	int count_chars = 0;
 	int format_current_index = 0, format_next_index = 0;
 	char current_char = 0, next_char = 0;
-	int *arr_result[2];
+	int arr_result[2];
 
 	while (format[format_current_index] != '\0')
 	{
-		indicator_index = 0;
 		format_next_index = format_current_index + 1;
 		current_char = format[format_current_index];
 		next_char = format[format_next_index];
 		if (current_char == '%')
 		{
-			arr_result = handle_indicator(next_char, arg_list,
-											format_current_index, count_chars);
+			handle_indicator(arr_result, next_char, arg_list,
+										format_current_index, count_chars);
 			count_chars	= arr_result[0];
 			format_current_index = arr_result[1];
 		}
