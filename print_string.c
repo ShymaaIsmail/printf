721ca193d65609225d7fd21a15542d0012662f82
shymaaismail <shymaa.m.ismail@gmail.com>
2023-09-17 11:41:45 +0300

    print (null) when the arg for %s is NULL

--- print_string.c
+++ print_string.c
@@ -1,4 +1,5 @@
 #include <stdarg.h>
+#include <stddef.h>
 #include "main.h"
 /**
 * print_string - print string
@@ -11,9 +12,11 @@ int print_string(va_list  string_arg)
 	int char_count = 0;
 	int iterator = 0;
 
+	if (string_arg != NULL)
+	{
 		char *string = (char *)va_arg(string_arg, char *);
 
-		if (*string != '\0')
+		if (string != NULL)
 		{
 			while (string[iterator] != '\0')
 			{
@@ -22,5 +25,26 @@ int print_string(va_list  string_arg)
 				iterator++;
 			}
 		}
+		else
+		{
+			_putchar('(');
+			_putchar('n');
+			_putchar('u');
+			_putchar('l');
+			_putchar('l');
+			_putchar(')');
+			char_count = 6;
+		}
+	}
+	else
+	{
+			_putchar('(');
+			_putchar('n');
+			_putchar('u');
+			_putchar('l');
+			_putchar('l');
+			_putchar(')');
+			char_count = 6;
+	}
 	return (char_count);
 }
